34335b2b410398bc437fee687a0c1c884f630812
Yurii Hordiienko <45203053+hordi@users.noreply.github.com>
2019-11-24 15:29:12 -0500

    added license-header

--- include/hash_set.h
+++ include/hash_set.h
@@ -1,3 +1,29 @@
+// Fast hashtable (hash_set, hash_map) based on open addressing hashing for C++11 and up
+// version 1.2.11
+// https://github.com/hordi/hash
+//
+// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2018-2019 Yurii Hordiienko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef hrd_hash_set_h_
 #define hrd_hash_set_h_
 
@@ -5,8 +31,6 @@
 #include <stdexcept>
 #include <cstdint>
 #include <string.h> //memcpy
-
-//version 1.2.11
 
 #ifdef _MSC_VER
 #  include <pmmintrin.h>
